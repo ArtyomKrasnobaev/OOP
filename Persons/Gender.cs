0548aeb00382ef94c40c4f812ebea48b7563f908
ArtyomKrasnobaev <artyom-red@yandex.ru>
2024-02-08 23:15:45 +0700

    feature(Persons): добавлены классы Person, Gender, PersonList

---
+++ Persons/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace Persons
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
--- Persons/Person.cs
+++ Persons/Person.cs
@@ -2,6 +2,49 @@
 {
     public class Person
     {
+        private string _firstName;
 
+        private string _lastName;
+
+        private int _age;
+
+        private Gender _gender;
+
+        public Person(string firstName, string lastName, int age, Gender gender)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+            _gender = gender;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public Gender Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string GetPersonInfo()
+        {
+            return $"Имя: {FirstName}, Фамилия: {LastName}, Возраст: {Age}, Пол: {Gender}\n";
+        }
     }
 }
