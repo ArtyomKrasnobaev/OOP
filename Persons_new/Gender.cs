1f8fc860a8d7ee05e7ac933230e03bb81b1ddd25
ArtyomKrasnobaev <artyom-red@yandex.ru>
2024-03-01 13:05:49 +0700

    fix: исправлены ошибки сборки

---
+++ Persons_new/Gender.cs
@@ -0,0 +1,18 @@
+﻿namespace Persons_new
+{
+    /// <summary>
+    /// Перечисление Gender.
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Мужской пол.
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// Женский пол.
+        /// </summary>
+        Female
+    }
+}
---
+++ Persons_new/PersonList.cs
@@ -0,0 +1,98 @@
+﻿namespace Persons_new
+{
+    /// <summary>
+    /// Класс PersonList.
+    /// </summary>
+    public class PersonList
+    {
+        /// <summary>
+        /// Список объектов класса Person.
+        /// </summary>
+        List<Person> personList = new List<Person>();
+
+        /// <summary>
+        /// Метод добавления объекта в список.
+        /// </summary>
+        /// <param name="person">Объект класса Person.</param>
+        public void AddPerson(Person person)
+        {
+            personList.Add(person);
+        }
+
+        /// <summary>
+        /// Метод удаления объекта из списка.
+        /// </summary>
+        /// <param name="person">Объект класса Person.</param>
+        public void RemovePerson(Person person)
+        {
+            personList.Remove(person);
+        }
+
+        /// <summary>
+        /// Метод удаления объекта из списка по индексу.
+        /// </summary>
+        /// <param name="index">Индекс объекта.</param>
+        public void RemovePersonByIndex(int index)
+        {
+            personList.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Метод получения объекта в списке по индексу.
+        /// </summary>
+        /// <param name="index">Индекс объекта.</param>
+        /// <returns>Объект класса Person.</returns>
+        public Person GetPersonByIndex(int index)
+        {
+            return personList[index];
+        }
+
+        /// <summary>
+        /// Метод получения индекса по объекту.
+        /// </summary>
+        /// <param name="person">Объект класса Person.</param>
+        /// <returns>Индекс объекта.</returns>
+        public int GetIndexOfPerson(Person person)
+        {
+            if (personList.Contains(person))
+            {
+                return personList.IndexOf(person);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Метод удаления объектов из списка.
+        /// </summary>
+        public void ClearList()
+        {
+            personList.Clear();
+        }
+
+        /// <summary>
+        /// Метод получения количества объектов списка.
+        /// </summary>
+        /// <returns>Количество объектов списка.</returns>
+        public int GetLength()
+        {
+            return personList.Count;
+        }
+
+        /// <summary>
+        /// Метод получения данных списка.
+        /// </summary>
+        /// <returns>Строка с данными полей объектов списка.</returns>
+        public string GetPersonListInfo()
+        {
+            string info = "";
+            foreach (Person person in personList)
+            {
+                info += person.GetPersonInfo();
+            }
+            return info;
+        }
+    }
+}
