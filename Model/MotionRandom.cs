2672d7a1ce3da2440462b15f0e6877589780d4e1
ArtyomKrasnobaev <artyom-red@yandex.ru>
2024-06-11 00:30:09 +0700

    dfdfd

--- Model/MotionRandom.cs
+++ Model/MotionRandom.cs
@@ -14,20 +14,72 @@ namespace Model
         private static Random _random = new Random();
 
         /// <summary>
-        /// Константа минимального значения.
+        /// Константа минимального значения рандома.
         /// </summary>
-        private const int _minValue = 0;
+        private const int _minValue = 1;
 
         /// <summary>
-        /// Константа максимального значения.
+        /// Константа максимального значения рандома.
         /// </summary>
         private const int _maxValue = 100;
 
+        public static double GetRandomDouble()
+        {
+            double randomDouble = _random.Next(_minValue, _maxValue);
+            return randomDouble;
+        }
+
+        public static MotionBase GetRandomMotion()
+        {
+            var motion = _random.Next(0, 2);
+            switch (motion)
+            {
+                case 0:
+                {
+                    return GetRandomConstantMotion();
+                }
+                case 1:
+                {
+                    return GetRandomAcceleratedMotion();
+                }
+                case 2:
+            }
+        }
+
+        public static void SetRandomMotionBaseData(MotionBase motionBase)
+        {
+            motionBase.InitialValue = GetRandomDouble();
+            motionBase.Time = GetRandomDouble();
+        }
+
+        /// <summary>
+        /// Метод генерации рандомного равномерного движения.
+        /// </summary>
+        /// <returns>Объект класса ConstantMotion.</returns>
         public static ConstantMotion GetRandomConstantMotion()
         {
             ConstantMotion constantMotion = new();
-            constantMotion.InitialValue = _random.Next(_minValue, _maxValue);
-            constantMotion.Velocity = _random.Next(_minValue, _maxValue);
+            SetRandomMotionBaseData(constantMotion);
+            constantMotion.Velocity = GetRandomDouble();
+            return constantMotion;
+        }
+
+        public static AcceleratedMotion GetRandomAcceleratedMotion()
+        {
+            AcceleratedMotion acceleratedMotion = new();
+            SetRandomMotionBaseData(acceleratedMotion);
+            acceleratedMotion.Velocity = GetRandomDouble();
+            acceleratedMotion.Acceleration = GetRandomDouble();
+            return acceleratedMotion;
+        }
+
+        public static OscillatoryMotion GetRandomOscillatoryMotion()
+        {
+            OscillatoryMotion oscillatoryMotion = new();
+            SetRandomMotionBaseData(oscillatoryMotion);
+            oscillatoryMotion.Amplitude = GetRandomDouble();
+            oscillatoryMotion.Frequency = GetRandomDouble();
+            return oscillatoryMotion;
         }
     }
 }
