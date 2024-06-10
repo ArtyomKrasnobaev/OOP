9274fb07d522d3854e0767e6b0175f937d5c278c
ArtyomKrasnobaev <artyom-red@yandex.ru>
2024-06-10 18:39:39 +0700

    dfdgdg

--- Model/MotionRandom.cs
+++ Model/MotionRandom.cs
@@ -8,14 +8,26 @@ namespace Model
 {
     internal class MotionRandom
     {
+        /// <summary>
+        /// Объект класса Random.
+        /// </summary>
         private static Random _random = new Random();
 
-        private const _minValue = 0;
+        /// <summary>
+        /// Константа минимального значения.
+        /// </summary>
+        private const int _minValue = 0;
 
+        /// <summary>
+        /// Константа максимального значения.
+        /// </summary>
+        private const int _maxValue = 100;
 
-        public ConstantMotion GetRandomConstantMotion()
+        public static ConstantMotion GetRandomConstantMotion()
         {
             ConstantMotion constantMotion = new();
+            constantMotion.InitialValue = _random.Next(_minValue, _maxValue);
+            constantMotion.Velocity = _random.Next(_minValue, _maxValue);
         }
     }
 }
