f2a23400f233daaa26ceba9c0d6bffc362d8713a
ArtyomKrasnobaev <artyom-red@yandex.ru>
2024-06-10 18:21:25 +0700

    dgdgd

---
+++ Model/MotionRandom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class MotionRandom
+    {
+        private static Random _random = new Random();
+
+        private const _minValue = 0;
+
+
+        public ConstantMotion GetRandomConstantMotion()
+        {
+            ConstantMotion constantMotion = new();
+        }
+    }
+}
